cc0244bd9c1ba5085f385a5b4f4ee636e36582b7
JS Morisset <jsm@surniaulula.com>
2021-11-15 18:52:30 -0800

    v1.1.0

--- wpsso-google-merchant-feed.php
+++ wpsso-google-merchant-feed.php
@@ -11,8 +11,8 @@
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Google Merchant Feeds for your WooCommerce, Easy Digital Downloads, and Custom Products (WPSSO Core Premium Required).
- * Requires PHP: 7.0
- * Requires At Least: 5.0
+ * Requires PHP: 7.2
+ * Requires At Least: 5.2
  * Tested Up To: 5.8.2
  * WC Tested Up To: 5.9.0
  * Version: 1.1.0
