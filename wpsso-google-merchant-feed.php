01ede374ce866fe10dabddd47643b97e77415900
JS Morisset <jsm@surniaulula.com>
2022-01-20 09:13:24 -0800

    v1.3.0

--- wpsso-google-merchant-feed.php
+++ wpsso-google-merchant-feed.php
@@ -13,8 +13,8 @@
  * Description: Google Merchant Feeds for WooCommerce, Easy Digital Downloads, and Custom Products (WPSSO Core Premium Required).
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 5.8.3
- * WC Tested Up To: 6.1.0
+ * Tested Up To: 5.9.0
+ * WC Tested Up To: 6.1.1
  * Version: 1.3.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
