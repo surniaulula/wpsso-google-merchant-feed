fd196b090b65e9bc9a4fbad002e8f6f5a8a73097
JS Morisset <jsm@surniaulula.com>
2022-12-11 07:37:15 -0800

    v3.4.0

--- wpsso-google-merchant-feed.php
+++ wpsso-google-merchant-feed.php
@@ -15,7 +15,7 @@
  * Requires PHP: 7.2
  * Requires At Least: 5.2
  * Tested Up To: 6.1.1
- * WC Tested Up To: 7.1.0
+ * WC Tested Up To: 7.1.1
  * Version: 3.4.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
