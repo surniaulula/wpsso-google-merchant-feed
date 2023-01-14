02f811b1eaffa9217da5501eec396c46d2199479
JS Morisset <jsm@surniaulula.com>
2023-01-13 16:07:06 -0800

    v5.1.0-b.2

--- lib/rewrite.php
+++ lib/rewrite.php
@@ -34,7 +34,7 @@ if ( ! class_exists( 'WpssoGmfRewrite' ) ) {
 			$this->p =& $plugin;
 			$this->a =& $addon;
 
-			add_action( 'wp_loaded', array( __CLASS__, 'add_rules' ), 1000 );
+			add_action( 'wp_loaded', array( __CLASS__, 'add_rules' ), 2000 );
 			add_action( 'template_redirect', array( __CLASS__, 'template_redirect' ), -2000 );
 
 			add_filter( 'query_vars', array( __CLASS__, 'query_vars' ) );
