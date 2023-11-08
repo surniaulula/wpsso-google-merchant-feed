667de6dbcfe94f80b326d3ec6dad8ff24feee713
JS Morisset <jsm@surniaulula.com>
2023-11-07 17:28:20 -0800

    v7.0.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoGmfConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssogmf' => array(			// Plugin acronym.
-					'version'     => '7.0.0-b.3',	// Plugin version.
+					'version'     => '7.0.0-rc.1',	// Plugin version.
 					'opt_version' => '2',		// Increment when changing default option values.
 					'short'       => 'WPSSO GMF',	// Short plugin name.
 					'name'        => 'WPSSO Google Merchant Feed XML',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoGmfConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '17.0.0-b.3',
+							'min_version'   => '17.0.0-rc.1',
 						),
 					),
 
