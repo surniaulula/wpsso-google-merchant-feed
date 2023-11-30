6f0d5a9c78162a5f27d54e81d8f845ea85b686e2
JS Morisset <jsm@surniaulula.com>
2023-11-29 17:41:16 -0800

    v9.0.0-b.1

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -6,3 +6,5 @@ _x( 'Google Merchant', 'lib file description', 'wpsso-google-merchant-feed' );
 _x( 'WPSSO Google Merchant Feed XML', 'plugin name', 'wpsso-google-merchant-feed' );
 _x( 'Google Merchant Feed XMLs for WooCommerce and custom product pages, with multilingual support.', 'plugin description', 'wpsso-google-merchant-feed' );
 _x( 'WPSSO Core', 'plugin name', 'wpsso-google-merchant-feed' );
+_x( 'Google Merchant Feed XML', 'option label', 'wpsso-google-merchant-feed' );
+_x( 'Google Merchant Inventory XML', 'option label', 'wpsso-google-merchant-feed' );
