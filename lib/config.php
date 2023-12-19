afcaf9a3644c70b6d16681bfdb6dfa5f104545ab
JS Morisset <jsm@surniaulula.com>
2023-12-18 20:45:07 -0800

    v9.3.0-b.3

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoGmfConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssogmf' => array(			// Plugin acronym.
-					'version'     => '9.3.0-b.2',	// Plugin version.
+					'version'     => '9.3.0-b.3',	// Plugin version.
 					'opt_version' => '4',		// Increment when changing default option values.
 					'short'       => 'WPSSO GMF',	// Short plugin name.
 					'name'        => 'WPSSO Google Merchant Feed XML',
