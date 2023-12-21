8041f47eb00c20e7ee84d2694642429afba8403d
JS Morisset <jsm@surniaulula.com>
2023-12-20 18:50:00 -0800

    v9.3.1-dev.1

--- lib/xml.php
+++ lib/xml.php
@@ -112,15 +112,22 @@ if ( ! class_exists( 'WpssoGmfXml' ) ) {
 				$wpsso->debug->mark( 'create feed' );	// Begin timer.
 			}
 
-			$site_title = SucomUtil::get_site_name( $wpsso->options, $request_locale );
-			$site_url   = SucomUtil::get_home_url( $wpsso->options, $request_locale );
-			$site_desc  = SucomUtil::get_site_description( $wpsso->options, $request_locale );
-			$query_args = array( 'meta_query' => WpssoAbstractWpMeta::get_column_meta_query_og_type( $og_type = 'product', $request_locale ) );
-			$public_ids = WpssoPost::get_public_ids( $query_args );
-			$rss2_feed  = new Vitalybaev\GoogleMerchant\Feed( $site_title, $site_url, $site_desc, '2.0' );
+			$site_title   = SucomUtil::get_site_name( $wpsso->options, $request_locale );
+			$site_url     = SucomUtil::get_home_url( $wpsso->options, $request_locale );
+			$site_desc    = SucomUtil::get_site_description( $wpsso->options, $request_locale );
+			$query_args   = array( 'meta_query' => WpssoAbstractWpMeta::get_column_meta_query_og_type( $og_type = 'product', $request_locale ) );
+			$public_ids   = WpssoPost::get_public_ids( $query_args );
+			$rss2_feed    = new Vitalybaev\GoogleMerchant\Feed( $site_title, $site_url, $site_desc, '2.0' );
+			$ship_enabled = empty( $wpsso->options[ 'gmf_add_shipping' ] ) ? false : true;
+
+			/*
+			 * Add or exclude the addition of shipping information to the Open Graph meta tags.
+			 */
+			add_filter( 'wpsso_og_add_mt_shipping_offers', ( $ship_enabled ? '__return_true' : '__return_false' ), 1000, 1 );
 
 			if ( $wpsso->debug->enabled ) {
 
+				$wpsso->debug->log( 'add shipping is ' . ( $ship_enabled ? 'enabled' : 'disabled' ) );
 				$wpsso->debug->log( 'adding ' . count( $public_ids ) . ' public ids' );
 				$wpsso->debug->log_arr( 'public_ids', $public_ids );
 				$wpsso->debug->mark_diff( 'adding ' . count( $public_ids ) . ' public ids' );
@@ -183,6 +190,11 @@ if ( ! class_exists( 'WpssoGmfXml' ) ) {
 			}
 
 			unset( $public_ids );
+
+			/*
+			 * Remove the filter hook, just in case.
+			 */
+			remove_filter( 'wpsso_og_add_mt_shipping_offers', ( $ship_enabled ? '__return_true' : '__return_false' ), 1000 );
 
 			if ( $wpsso->debug->enabled ) {
 
@@ -263,11 +275,6 @@ if ( ! class_exists( 'WpssoGmfXml' ) ) {
 					 * See https://support.google.com/merchants/answer/7052112?hl=en#shipping_and_returns.
 					 */
 					if ( ! empty( $wpsso->options[ 'gmf_add_shipping' ] ) ) {
-
-						if ( $wpsso->debug->enabled ) {
-
-							$wpsso->debug->log( 'add shipping is enabled' );
-						}
 
 						self::add_item_shipping( $item, $mt_single );
 					}
