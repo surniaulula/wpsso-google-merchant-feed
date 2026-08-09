7352a333c7b1a8985497445c4e3bc1bdcc549908
jsmoriss <jsm@surniaulula.com>
2026-08-09 16:17:20 +0000

    v9.14.0

--- wpsso-google-merchant-feed.php
+++ wpsso-google-merchant-feed.php
@@ -15,7 +15,7 @@
  * Requires PHP: 7.4.33
  * Requires At Least: 6.0
  * Tested Up To: 7.0.2
- * WC Tested Up To: 10.9.4
+ * WC Tested Up To: 11.0.0
  * Version: 9.14.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
